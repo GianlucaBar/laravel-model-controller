af47e04baa9a9e1b99565e181f3a3fcf778330a8
GianlucaBar <gianlucabarce@gmail.com>
2021-06-10 17:45:36 +0200

    completato

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends Controller
     public function index(){
 
         $movies = Movie::all();
-        dd($movies);
 
         $data =[
             'movies' => $movies
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,57 @@
     <title>Movies</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>welcome</h1>
+
+    <header>
+        <h1>MOVIES LIST</h1>
+    </header>
+
+    <main>
+        <div class="movies-container">
+            @foreach ($movies as $movie)
+            <div class="movie-card">
+                <h2>{{ $movie->title }}</h2>
+
+                <div class="card-row">
+
+                    <div class="row-title">Original title:</div>
+
+                    <div>{{ $movie->original_title }}</div>
+
+                </div>
+
+                <div class="card-row">
+                    
+                    <div class="row-title">Nationality:</div>
+
+                    <div>{{ $movie->nationality }}</div>
+
+                </div>
+
+                <div class="card-row">
+                    
+                    <div class="row-title">Date:</div>
+
+                    <div>{{ $movie->date }}</div>
+
+                </div>
+
+                <div class="card-row">
+                    
+                    <div class="row-title">Vote:</div>
+
+                    <div>{{ $movie->vote }}</div>
+
+                </div>
+    
+            </div>
+        @endforeach
+        </div>
+    </main>
+
 </body>
 </html>
